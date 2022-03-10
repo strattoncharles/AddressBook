448320970a42567e26e9c5251a42b80accfa495d
strattoncharles <98862282+strattoncharles@users.noreply.github.com>
2022-03-09 17:18:55 -0800

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using AddressBook.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AddressBook.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
---
+++ Services/BasicImageService.cs
@@ -0,0 +1,29 @@
+﻿using AddressBook.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace AddressBook.Services
+{
+    public class BasicImageService : IImageService
+    {
+        public string ConvertByteArrayToFile(byte[] filedata, string extenstion)
+        {
+            if (filedata is null) return string.Empty;
+
+            string imageBase64Data = Convert.ToBase64String(filedata);
+            return $"data:{extenstion};based64,{imageBase64Data}";
+        }
+
+        public async Task<byte[]> ConvertFileToByteArrayAsync(IFormFile file)
+        {
+            using MemoryStream memoryStream = new();
+            await file.CopyToAsync(memoryStream);
+            byte[] byteFile = memoryStream.ToArray();
+
+            return byteFile;
+
+        }
+    }
+}
